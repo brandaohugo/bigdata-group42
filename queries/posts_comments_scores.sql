b3e5c2dd7609fd116f079a7b41abc85d87093945
Hugo Brandao <hugo@feelrobotics.com>
2021-03-12 14:42:52 +0100

    add sql files

---
+++ queries/posts_comments_scores.sql
@@ -0,0 +1,5 @@
+SELECT p.Id, p.Title, p.Body, c.Score, c.Text
+FROM posts as p, comments as c
+WHERE p.Id = c.PostId
+ORDER BY p.Id
+LIMIT 10000;
---
+++ queries/user_badges.sql
@@ -0,0 +1,4 @@
+SELECT u.DisplayName, b.Name 
+FROM users as u, badges as b 
+WHERE b.id = u.id 
+LIMIT 1000;
